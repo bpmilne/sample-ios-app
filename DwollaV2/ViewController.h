51b8a2592738e939520dd29bf0bf2fbb817ee855
Nick Schulze <nschulze16@gmail.com>
2013-02-14 15:04:02 -0600

    Initial Commit

---
+++ DwollaV2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DwollaV2
+//
+//  Created by Nick Schulze on 2/14/13.
+//  Copyright (c) 2013 Nick Schulze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DwollaV2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DwollaV2
+//
+//  Created by Nick Schulze on 2/14/13.
+//  Copyright (c) 2013 Nick Schulze. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
